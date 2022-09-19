d679645750cc72610c062ad2b667d08a6fd61aea
m-avagyan <martikavagyan1@gmail.com>
2022-09-19 14:44:57 +0400

    feat: improve pkg performance

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import useIsMounted from './is-mounted.hook';
+import { useRef, useEffect, useMemo } from 'react';
+
+function useIsMounted(): boolean {
+  const isMountedRef = useRef(false);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  const isMounted = useMemo(() => isMountedRef.current, [isMountedRef]);
+
+  return isMounted;
+}
 
 export default useIsMounted;
