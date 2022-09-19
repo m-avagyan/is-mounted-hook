c75e685695f085ef9a18a45d8b2bb4b74eb78f84
m-avagyan <martikavagyan1@gmail.com>
2022-09-19 14:44:40 +0400

    wip: remove test files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)test)\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
